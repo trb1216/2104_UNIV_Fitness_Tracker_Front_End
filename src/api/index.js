6b1aa67b5d7ff840314c043da3e9d6e212fcd7eb
trb1216 <trb1216@gmail.com>
2021-09-16 20:18:33 -0500

    renit without my-app folder

---
+++ src/api/index.js
@@ -0,0 +1,81 @@
+// eslint-disable-next-line no-unused-expressions
+// remember basic promises:
+// fetch('http://fitnesstrac-kr.herokuapp.com/api/some-endpoint')
+// .then(res => {
+// if (res.ok) {
+//     console.log('fetching')
+// } else {
+//     console.log('404')
+// }
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log('..error'));
+// Ben: Window.localStorage.setItem("token", token) add to data. 
+
+const token =
+  "none of your business";
+fetch("http://fitnesstrac-kr.herokuapp.com/api/users/register", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    username: "username",
+    password: "password",
+  }),
+})
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log("..error"));
+
+fetch("http://fitnesstrac-kr.herokuapp.com/api/users/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    username: "username",
+    password: "password",
+  }),
+})
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log("..error"));
+
+fetch("http://fitnesstrac-kr.herokuapp.com/api/users/me", {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+})
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log("..error"));
+
+// const URL = "https://fitnesstrac-kr.herokuapp.com/api";
+
+// export const register = async (username, password) => {
+
+//     const body = {
+//         user: {
+//           username,
+//           password,
+//         },
+//       };
+//       const fetchArgs = {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(body),
+//       };
+// const res = await fetch(URL + "users/register", fetchArgs);
+// const json = await res.json();
+// console.log(json);
+// return json ;
---
+++ src/components/Activities.jsx
@@ -0,0 +1,8 @@
+// As an unregistered visitor on the Activities tab, I want to:
+
+// see a list of all activities which have been created
+// Probably same logic as getting all posts in stranger things?
+// As a registered user on the Activities tab, I want to:
+
+// be shown a form to create a new activity (by name and description)
+// be shown an error if the activity already exists
